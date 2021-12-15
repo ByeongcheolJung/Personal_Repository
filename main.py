0409b523fd26bd9acb24195e17dbaff5b1b511d6
San_Sam <qudcjf8349@gmail.com>
2021-12-15 09:20:23 +0900

    테스트

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def print_hi(name):
+    print(f'Hi, {name}1')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}2')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}3')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}4')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}5')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
