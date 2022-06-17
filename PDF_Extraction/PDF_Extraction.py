f3241e0415511c9cf8048f28d05c33ebc3813a14
sansam <qudcjf8349@gmail.com>
2022-06-17 09:46:23 +0900

    06-17 최신화

--- PDF_Extraction/PDF_Extraction.py
+++ PDF_Extraction/PDF_Extraction.py
@@ -4,35 +4,21 @@
 filename = 'C:\\Users\\Gurutech\\Documents\\Personal_Repository\\PDF_Extraction\\Sample_data\\[약식의결서(안)]_수정.pdf'
 
 
-import PyPDF2
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+for page_layout in extract_pages(filename):
+    #text는 단순 데이터 받고, check는 유형 6가지 확인하기 위한것
+    text = []
+    check = []
+    for element in page_layout:
+        if isinstance(element, LTTextContainer):
+            #text값이 비어있으면 스킵
+            if element.get_text() == []:
+                continue
+            else:
+                text.append(element.get_text())
 
+                print(text)
 
-file = open(filename, 'rb')
-fileReader = PyPDF2.PdfFileReader(file)
-
-fileReader.pageMode
-
-# 문서의 정보를 읽어드린다
-fileReader.documentInfo
-
-# 전체 페이지수를 출력한다
-print("page : ",fileReader.numPages)
-
-# 첫 번째 페이지 정보를 가져온다
-pageObj = fileReader.getPage(0)
-
-# 페이지 정보의 텍스트를 가져온다
-text = pageObj.extractText()
-name =""
-
-
-
-
-print("text : ",text)
-
-try:
-    name = name.decode('utf-8')
-except:
-    pass
-
-print("name : ", name)
+        #text초기화
+        text = []
