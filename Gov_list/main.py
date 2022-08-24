83479f4ae44aea6851a8dada1cf52074b9324a80
ByeongcheolJung <terren_ghost@naver.com>
2022-08-24 13:57:01 +0900

    08-24 / 평가 대상인 기관만 뽑도록 설정, 저장경로까지 추가적으로 표시

--- Gov_list/main.py
+++ Gov_list/main.py
@@ -1,5 +1,5 @@
 import time
-
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -7,17 +7,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 import re
 from openpyxl import load_workbook
 
-
-#shutil.copy('양식.xlsx', '기관목록 결과.xlsx')
-
 workbook = load_workbook('양식.xlsx')
 worksheet = workbook.active
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://gooddata.go.kr/dqe/account/login')
 driver.implicitly_wait(30)
-#아이디 비밀번호 입력후 로그인
-#로그인 화면 이동
+
+#아이디 비밀번호 입력후 로그인 화면 이동
 driver.find_element(By.ID, 'username').send_keys("sysMaster")
 driver.find_element(By.ID, 'password').send_keys("sys!234%")
 driver.find_element(By.CLASS_NAME, 'login-form-btn').click()
@@ -29,14 +26,16 @@ driver.implicitly_wait(10)
 #대상여부 O만 뽑을떄!!!
 #https://gooddata.go.kr/dqe/admin/databases?searchYear=2022&isTarget=true&isSelected=&dbSelectionStatus=&institutionType=&institutionCode=&insttName=
 
-
 #전체 기관 뽑을떄!!!
-driver.get('https://gooddata.go.kr/dqe/admin/databases?page=0&searchYear=2022&')
+#driver.get('https://gooddata.go.kr/dqe/admin/databases?page=0&searchYear=2022&')
+
+#평가대상 기관만 뽑을떄!!!
+driver.get('https://gooddata.go.kr/dqe/admin/databases?searchYear=2022&isTarget=true&isSelected=&dbSelectionStatus=&institutionType=&institutionCode=&insttName=')
 
 #결과값 갯수
 search_result = driver.find_element(By.XPATH, '//*[@id="app"]/div[4]/div/div[1]/span').text
-print('결과값 : '+search_result)
-print('정제값 : '+ re.sub(r'[^0-9]','',search_result))
+#print('결과값 : '+search_result)
+print('결과값 : '+ re.sub(r'[^0-9]','',search_result)) #숫자만 추출
 search_res = int(re.sub(r'[^0-9]','',search_result))
 
 #데이터 임시 저장할곳
@@ -45,19 +44,21 @@ data_array = [[0]*19 for i in range(search_res)]
 #페이지 갯수
 pageNum = int(search_res/10)
 stoppoint = True
-#데이터 저장
 
+#데이터 저장
 for page in range(0,pageNum+1):
-        print(page+1)
+        print(str(page+1) + "P / " + str(pageNum+1) + "P")
         #확정상태 뽑을떄!!!
         #driver.get('https://gooddata.go.kr/dqe/admin/databases?page='+str(page)+'&searchYear=2022&dbSelectionStatus=SELECTED&institutionCode=')
 
         #대상여부가 O인 기관만 뽑을떄!!!
         #driver.get('https://gooddata.go.kr/dqe/admin/databases?page='+str(page)+'&searchYear=2022&isTarget=true&institutionCode=')
 
-        #대상기괸 다 뽑을떄!!!
-        driver.get('https://gooddata.go.kr/dqe/admin/databases?page='+str(page)+'&searchYear=2022&')
+        #모든기괸 다 뽑을떄!!!
+        #driver.get('https://gooddata.go.kr/dqe/admin/databases?page='+str(page)+'&searchYear=2022&')
 
+        #평가대상인 기관만 뽑을떄!!!
+        driver.get('https://gooddata.go.kr/dqe/admin/databases?page='+str(page)+'&searchYear=2022&isTarget=true&institutionCode=')
 
         driver.implicitly_wait(10)
         for i in range(0,10):
@@ -66,7 +67,7 @@ for page in range(0,pageNum+1):
                 try:
                         data_array[i + page * 10][0] = i + page*10+1
                 except:
-                        print("페이지 크롤링 끝")
+                        print("크롤링 끝!!!")
                         stoppoint = False
                         break
 
@@ -94,9 +95,9 @@ for page in range(0,pageNum+1):
         else:
                 break
 
-
 for x in range(0,search_res):
-        print(data_array[x])
+        #결과 표시
+        #print(data_array[x])
         for y in range(0, 19):
                 worksheet.cell(row = 3+x,column=y+1, value=data_array[x][y])
 
@@ -124,6 +125,8 @@ if time.localtime().tm_min<10:
 else:
         today_min = str(time.localtime().tm_min)
 
+print("[저장위치] >>> "+os.getcwd()+'\CRAWLING-RESULT_'+today_month+today_day+'_'+today_hour+today_min+'.xlsx'+" 저장완료!!!")
 
 workbook.save('CRAWLING-RESULT_'+today_month+today_day+'_'+today_hour+today_min+'.xlsx')
 workbook.close()
+
